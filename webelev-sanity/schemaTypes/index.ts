b078089f92d74aaa5098fc35cf2bbf9a76906785
pcdev-90 <65405085+pcdev-90@users.noreply.github.com>
2025-01-10 02:32:04 -0300

    first-commit

---
+++ webelev-sanity/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import template from './template';
+export const schemaTypes = [post, author, category, blockContent, template]
